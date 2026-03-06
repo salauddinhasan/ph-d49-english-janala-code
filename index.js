11ea71719576e8fe33755eba810be89d58fb0191
salauddin <salauddin@hasan.com>
2026-03-06 12:06:24 +0600

    add pronunciation feature for words

--- index.js
+++ index.js
@@ -3,6 +3,12 @@
     return(htmlElement.join(' '));
 };
 
+function pronounceWord(word) {
+  const utterance = new SpeechSynthesisUtterance(word);
+  utterance.lang = "en-EN"; // English
+  window.speechSynthesis.speak(utterance);
+}
+
 const manageSpinner = (status) => {
   if(status == true) {
     document.getElementById('spinner').classList.remove('hidden');
@@ -105,7 +111,7 @@ const displayWordDetails = (word) => {
               <button onclick="loadWordDetail(${word.id})" class="btn bg-[#1A91FF10] hover:bg-[#1a91ff80]">
                 <i class="fa-solid fa-circle-info"></i>
               </button>
-              <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF70]">
+              <button onclick="pronounceWord('${word.word}')" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF70]">
                 <i class="fa-solid fa-volume-high"></i>
               </button>
             </div>
